c0fc238a3558248de7b0398023b420bc6c488a62
AbdulHannKhan <108698579+AbdulHannKhan@users.noreply.github.com>
2023-01-08 22:07:08 +0500

    header & bottom sheet button fixed

--- src/screens/Goals/UpdateProfile.js
+++ src/screens/Goals/UpdateProfile.js
@@ -82,8 +82,214 @@ import { PoppinsRegular, PoppinsSemiBold } from '../../../assets/fonts/Fonts'
 import EditProfileButton from '../../components/EditProfileButton'
 import { useNavigation } from '@react-navigation/native'
 import UpdateInput from '../../components/UpdateInput'
+import SelectList from 'react-native-dropdown-select-list';
+import Feather from 'react-native-vector-icons/Feather'
+import FontAwesome from 'react-native-vector-icons/FontAwesome'
+
 
 const EditProfile = (props) => {
+    const data = [
+        //   {key:'1',value:'Select One'},
+        { value: 'Afghanistan' },
+        { value: 'Albania' },
+        { value: 'Algeria' },
+        { value: 'Angola' },
+        { value: 'Antigua and Barbuda' },
+        { value: 'Argentina' },
+        { value: 'Armenia' },
+        { value: 'Australia' },
+        { value: 'Austria' },
+        { value: 'Azerbaijan	' },
+        { value: 'Bahamas' },
+        { value: 'Bahrain' },
+        { value: 'Bangladesh' },
+        { value: 'Barbados' },
+        { value: 'Belarus' },
+        { value: 'Belgium' },
+        { value: 'Belize' },
+        { value: 'Benin' },
+        { value: 'Bhutan' },
+        { value: 'Bolivia' },
+        { value: 'Bosnia and Herzegovina	' },
+        { value: 'Botswana' },
+        { value: 'Brazil' },
+        { value: 'Brunei' },
+        { value: 'Bulgaria' },
+        { value: 'Burkina Faso	' },
+        { value: 'Burundi' },
+        { value: 'Côte dIvoire' },
+        { value: 'Cabo Verde' },
+        { value: 'Cambodia' },
+        { value: 'Cameroon' },
+        { value: 'Canada' },
+        { value: 'Central African Republic	' },
+        { value: 'Chad' },
+        { value: 'Chile' },
+        { value: 'China' },
+        { value: 'Colombia' },
+        { value: 'Comoros' },
+        { value: 'Congo (Congo-Brazzaville)	' },
+        { value: 'Costa Rica	' },
+        { value: 'Croatia' },
+        { value: 'Cuba' },
+        { value: 'Cyprus' },
+        { value: 'Czechia (Czech Republic)	' },
+        { value: 'Democratic Republic of the Congo' },
+        { value: 'Denmark' },
+        { value: 'Djibouti' },
+        { value: 'Dominica' },
+        { value: 'Dominican Republic	' },
+        { value: 'Ecuador' },
+        { value: 'Egypt' },
+        { value: 'El Salvador	' },
+        { value: 'Equatorial Guinea	' },
+        { value: 'Eritrea' },
+        { value: 'Estonia' },
+        { value: 'Eswatini (fmr. "Swaziland")	' },
+        { value: 'Ethiopia' },
+        { value: 'Fiji' },
+        { value: 'Finland' },
+        { value: 'France' },
+        { value: 'Gabon' },
+        { value: 'Gambia' },
+        { value: 'Georgia' },
+        { value: 'Germany' },
+        { value: 'Ghana' },
+        { value: 'Greece' },
+        { value: 'Grenada' },
+        { value: 'Guatemala' },
+        { value: 'Guinea' },
+        { value: 'Guinea-Bissau	' },
+        { value: 'Guyana' },
+        { value: 'Haiti' },
+        { value: 'Holy See	' },
+        { value: 'Honduras' },
+        { value: 'Hungary' },
+        { value: 'Iceland' },
+        { value: 'India' },
+        { value: 'Indonesia' },
+        { value: 'Iran' },
+        { value: 'Iraq' },
+        { value: 'Ireland' },
+        { value: 'Israel' },
+        { value: 'Italy' },
+        { value: 'Jamaica' },
+        { value: 'Japan' },
+        { value: 'Jordan' },
+        { value: 'Kazakhstan' },
+        { value: 'Kenya' },
+        { value: 'Kiribati' },
+        { value: 'Kuwait' },
+        { value: 'Kyrgyzstan' },
+        { value: 'Laos' },
+        { value: 'Latvia' },
+        { value: 'Lebanon' },
+        { value: 'Lesotho' },
+        { value: 'Liberia' },
+        { value: 'Libya' },
+        { value: 'Liechtenstein' },
+        { value: 'Lithuania' },
+        { value: 'Luxembourg' },
+        { value: 'Madagascar' },
+        { value: 'Malawi' },
+        { value: 'Malaysia' },
+        { value: 'Maldives' },
+        { value: 'Mali' },
+        { value: 'Malta	' },
+        { value: 'Marshall Islands	' },
+        { value: 'Mauritania' },
+        { value: 'Mauritius' },
+        { value: 'Mexico' },
+        { value: 'Micronesia' },
+        { value: 'Moldova' },
+        { value: 'Monaco' },
+        { value: 'Mongolia' },
+        { value: 'Montenegro' },
+        { value: 'Morocco' },
+        { value: 'Mozambique' },
+        { value: 'Myanmar (formerly Burma)	' },
+        { value: 'Namibia' },
+        { value: 'Nauru' },
+        { value: 'Nepal' },
+        { value: 'Netherlands' },
+        { value: 'New Zealand	' },
+        { value: 'Nicaragua' },
+        { value: 'Niger' },
+        { value: 'Nigeria' },
+        { value: 'North Korea	' },
+        { value: 'North Macedonia	' },
+        { value: 'Norway' },
+        { value: 'Oman' },
+        { value: 'Pakistan' },
+        { value: 'Palau' },
+        { value: 'Palestine State	' },
+        { value: 'Panama' },
+        { value: 'Papua New Guinea	' },
+        { value: 'Paraguay' },
+        { value: 'Peru' },
+        { value: 'Philippines' },
+        { value: 'Poland' },
+        { value: 'Portugal' },
+        { value: 'Qatar' },
+        { value: 'Romania' },
+        { value: 'Russia' },
+        { value: 'Rwanda' },
+        { value: 'Saint Kitts and Nevis	' },
+        { value: 'Saint Lucia	' },
+        { value: 'Saint Vincent and the Grenadines	' },
+        { value: 'Samoa' },
+        { value: 'San Marino	' },
+        { value: 'Sao Tome and Principe	' },
+        { value: 'Saudi Arabia	' },
+        { value: 'Senegal' },
+        { value: 'Serbia' },
+        { value: 'Seychelles' },
+        { value: 'Sierra Leone' },
+        { value: 'Singapore' },
+        { value: 'Slovakia' },
+        { value: 'Slovenia' },
+        { value: 'Solomon Islands' },
+        { value: 'Somalia' },
+        { value: 'South Africa' },
+        { value: 'South Korea' },
+        { value: 'South Sudan' },
+        { value: 'Spain' },
+        { value: 'Sri Lanka	' },
+        { value: 'Sudan' },
+        { value: 'Suriname' },
+        { value: 'Sweden' },
+        { value: 'Switzerland' },
+        { value: 'Syria' },
+        { value: 'Tajikistan' },
+        { value: 'Tanzania' },
+        { value: 'Thailand' },
+        { value: 'Timor-Leste	' },
+        { value: 'Togo' },
+        { value: 'Tonga' },
+        { value: 'Trinidad and Tobago' },
+        { value: 'Tunisia	' },
+        { value: 'Turkey' },
+        { value: 'Turkmenistan	' },
+        { value: 'Tuvalu' },
+        { value: 'Uganda' },
+        { value: 'Ukraine' },
+        { value: 'United Arab Emirates	' },
+        { value: 'United Kingdom	' },
+        { value: 'United States of America	' },
+        { value: 'Uruguay' },
+        { value: 'Uzbekistan' },
+        { value: 'Vanuatu' },
+        { value: 'Venezuela' },
+        { value: 'Vietnam' },
+        { value: 'Yemen' },
+        { value: 'Zambia' },
+        { value: 'Zimbabwe' },
+    ];
+
+
+
+    const [selected, setSelected] = React.useState('');
+
     const navigation = useNavigation();
 
     const userData = props.route.params.userData;
@@ -104,13 +310,36 @@ const EditProfile = (props) => {
                         <EditDetails detail1={userData.questions.jobTitle} detail2={company?.companyName} title={userData.name} source={edit} name='kuchbhi' />
                     </View>
                     <EditProfileButton onPress={() => navigation.navigate('updateprofile')} EditText={'Update'} edit={'hehe'} />
-                    <UpdateInput placeholder={'021 456 789'} title={'Phone Number'} />
-                    <UpdateInput placeholder={'Pakistan'} title={'Location'} />
-                    
+                    <UpdateInput placeholder={'021 456 789'} title={'Phone Number'} line={'aa'}/>
+                    {/* <UpdateInput placeholder={'Pakistan'} title={'Location'} /> */}
+
+
+                    <View style={styles.phoneView}>
+                        <Text style={styles.text}>Your Location</Text>
+
+                        <SelectList
+                            style={styles.inside}
+                            //   onSelect={() => alert(selected)}
+                            setSelected={setSelected}
+                            data={data}
+                            arrowicon={
+                                <Feather name="chevron-down" size={15} color={'black'} />
+                            }
+                            searchicon={<FontAwesome name="search" size={12} color={'black'} />}
+                            search={true}
+                            maxHeight="100"
+                            boxStyles={styles.boxStyle} //override default styles
+                            defaultOption={{ value: 'Select ' }} //default selected option
+                            dropdownStyles={styles.dropdownStyles}
+                            dropdownTextStyles={styles.dropdownTextStyles}
+                            dropdownItemStyles={styles.dropdownItemStyles}
+                        />
+                    </View>
+                    {/* <View style={{ width: '200%', height: '0.2%', backgroundColor: '#E0E0E0', marginTop: verticalScale(20) }}></View> */}
                     <EditButtons cross={'aa'} input={'showInput'} addIcon={'aa'} title='Tools' addImage={add} properties={['Adobe XD', 'Miro', 'Illustrator', 'Figma', 'Photoshop']} />
                     <EditButtons cross={'aa'} input={'showInput'} addIcon={'aa'} title='Rockstar Skills' addImage={add} properties={userData.questions.rockstarSkills} />
                     <EditButtons cross={'aa'} input={'showInput'} addIcon={'aa'} title='When you are not working, what do you enjoy doing the most?' addImage={add} properties={userData.questions.Hobbies} />
-                    <UpdateInput placeholder={'Lorem ipsum dolor sit amet'} title={'How Would you describe job to the group of 5 years old?'} />
+                    <UpdateInput placeholder={'Lorem ipsum dolor sit amet'} title={'How Would you describe job to the group of 5 years old?'} line={'ss'} />
                     <UpdateInput placeholder={'Lorem ipsum dolor sit amet'} title={'What is ONE of the coolest things you have ever done?'} line={'aa'} />
 
                 </View>
@@ -160,7 +389,34 @@ const styles = StyleSheet.create({
         borderRadius: moderateScale(65),
 
 
-    }
+    },
+    phoneView: {
+        marginTop: verticalScale(20),
+    },
+    text: {
+        color: 'black',
+        fontSize: moderateScale(18),
+        fontFamily: PoppinsSemiBold,
+        marginHorizontal: scale(20),
+        // marginBottom:-10
+    },
+    dropdownStyles: {
+        backgroundColor: 'white',
+        color: 'black',
+        borderColor: 'white',
+        marginTop: 0,
+        marginHorizontal: scale(20),
+    },
+    dropdownTextStyles: {
+        color: 'black',
+    },
+    boxStyle: {
+        backgroundColor: 'white',
+        placeholderTextColor: 'black',
+        borderColor: 'white',
+        borderRadius: moderateScale(50),
+        marginHorizontal: scale(20),
+    },
 })
 
 
