9c2ccc91ab798022d2372c6373f7f31175a827aa
MuhammadShahudd <shahud@plumtreegroup.net>
2023-07-26 21:12:53 +0500

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Ensemble
 # EnsembleIOS
+# EnsembleIOS
---
+++ ios/JoinEnsemble/JoinEnsembleApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoinEnsembleApp.swift
+//  JoinEnsemble
+//
+//  Created by Muhammad Shahud on 28/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct JoinEnsembleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
